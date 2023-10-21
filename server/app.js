43e5a5ebc6c9be4c8e85fc65a18be5c6ae5bb692
Shaya Ginzburg <69621958+ShayaG94@users.noreply.github.com>
2023-10-21 18:59:36 +0300

    Fix server name on external package.json (#7)
    * Fix server name on externak package.json
    
    * Add basic routes to server
    
    * Establish basic mongo connection
    
    * Create mongo schemas

--- server/app.js
+++ server/app.js
@@ -1,10 +1,49 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+mongoose.set("strictQuery", true);
+mongoose.connect("mongodb://localhost:27017/my_recipes");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-    res.send("Welcome my_recepie by K&G productions!");
+    res.send("Welcome my_recipes by K&G productions!");
+});
+
+app.get("/myrecipes", async (req, res) => {
+    res.send("<h1>My Recipes</h1>");
+});
+
+app.get("/myrecipes/new", (req, res) => {
+    res.send("New recipe form");
+});
+
+app.post("/myrecipes", async (req, res) => {
+    res.send(`Submitted new recipe: ${req.body}`);
+});
+
+app.get("/myrecipes/:id", async (req, res) => {
+    const { id } = req.params;
+    res.send(`Show one recipe with this ID: ${id}`);
+});
+
+app.put("/myrecipes/:id", async (req, res) => {
+    const { id } = req.params;
+    res.send(`Editing a recipe with ID: ${id}`);
+});
+
+app.delete("/myrecipes/:id", async (req, res) => {
+    const { id } = req.params;
+    res.send(`Deleting a recipe with ID: ${id}`);
 });
 
 app.listen(3300, () => {
---
+++ server/models/recipe.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const Line = require("./line");
+const Comment = require("./comment");
+
+const RecipeSchema = new Schema({
+    title: String,
+    content: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Line",
+        },
+    ],
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Comment",
+        },
+    ],
+});
+
+RecipeSchema.post("findOneAndDelete", async function (doc) {
+    if (doc) {
+        await Line.deleteMany({
+            _id: {
+                $in: doc.content,
+            },
+        });
+        await Comment.deleteMany({
+            _id: {
+                $in: doc.comments,
+            },
+        });
+    }
+});
+
+module.exports = mongoose.model("Campground", CampgroundSchema);
